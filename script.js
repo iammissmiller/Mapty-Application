212bea17835cda84844f1c7bf9df95c221f08e49
Praptee Miller <139339934+iammissmiller@users.noreply.github.com>
2025-11-09 03:23:16 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -17,6 +17,29 @@ navigator.geolocation.getCurrentPosition(function(position) {
     const {latitude} = position.coords;
     const {longitude} = position.coords;
     console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
-} , function() {
+
+    const coords = [latitude, longitude];
+
+const map = L.map('map').setView(coords, 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+map.on('click' , function(mapEvent) {
+
+    const {lat, lng} = mapEvent.latlng;
+
+    L.marker([lat , lng]).addTo(map)
+    .bindPopup(L.popup())
+    .openPopup();
+
+})
+
+}
+ , function() {
     alert("Could not get your Position");
 })
+
+
+
